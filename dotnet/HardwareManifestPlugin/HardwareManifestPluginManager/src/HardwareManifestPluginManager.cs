13abf9e9eb14d8e532efee37f303d3b111238351
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2022-06-13 16:08:25 +0000

    Changed constructor

--- dotnet/HardwareManifestPlugin/HardwareManifestPluginManager/src/HardwareManifestPluginManager.cs
+++ dotnet/HardwareManifestPlugin/HardwareManifestPluginManager/src/HardwareManifestPluginManager.cs
@@ -5,13 +5,12 @@ using System.Reflection;
 
 namespace HardwareManifestPluginManager {
     public class HardwareManifestPluginManager {
+        private static readonly ILogger log = Log.ForContext<HardwareManifestPluginManager>();
+
         public static readonly string pluginsPath = Path.Combine(Path.GetDirectoryName(Environment.ProcessPath), "plugins");
         public static readonly string trustPath = Path.Combine(Path.GetDirectoryName(Environment.ProcessPath), "trust");
-        public HardwareManifestPluginManager(ILogger logger) {
-            Log.Logger = logger;
-        }
         
-        public List<IHardwareManifest> LoadPlugins(List<string> names, bool swidEnforced) {
+        public static List<IHardwareManifest> LoadPlugins(List<string> names, bool swidEnforced) {
             string[] pluginDlls = System.IO.Directory.GetFiles(pluginsPath, "*.dll");
             List<IHardwareManifest> manifests = new();
             foreach(string dllPath in pluginDlls) {
@@ -24,12 +23,12 @@ namespace HardwareManifestPluginManager {
                     }
                     if (trustManifest) {
                         manifests.Add(manifest);
-                        Log.Warning("Loading hardware manifest: " + manifest.Name);
+                        log.Warning("Loading hardware manifest: " + manifest.Name);
                     }
                 }
             }
             if (names.Count > 0) {
-                Log.Warning("There was no Hardware Manifest plugin with the name " + (names.Count > 1 ? "s" : "") + string.Join(",", names) + ".");
+                log.Warning("There was no Hardware Manifest plugin with the name " + (names.Count > 1 ? "s" : "") + string.Join(",", names) + ".");
             }
             return manifests;
         }
@@ -37,7 +36,7 @@ namespace HardwareManifestPluginManager {
         private static Assembly LoadAssemblyfromDll(string relativePath) {
             string fullPath = Path.GetFullPath(relativePath).Replace('\\', Path.DirectorySeparatorChar);
 
-            Log.Warning($"Seeing if this assembly implements IHardwareManifest: {fullPath}");
+            log.Warning($"Seeing if this assembly implements IHardwareManifest: {fullPath}");
             PluginLoadContext loadContext = new(fullPath);
             return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(fullPath)));
         }
@@ -46,17 +45,17 @@ namespace HardwareManifestPluginManager {
             foreach (Type type in assembly.GetTypes()) {
                 if (typeof(IHardwareManifest).IsAssignableFrom(type)) {
                     if (Activator.CreateInstance(type) is IHardwareManifest result && names.Remove(result.Name)) {
-                        Log.Warning("Found " + result.Name + ".");
+                        log.Warning("Found " + result.Name + ".");
                         return result;
                     }
                 }
             }
-            Log.Warning($"Can't find any type which implements IHardwareManifest in {assembly}.\n");
+            log.Warning($"Can't find any type which implements IHardwareManifest in {assembly}.\n");
             return null;
         }
 
         private static bool VerifySWIDWithEnvelopedSignature(SoftwareIdentity SWID) {
-            Log.Warning("SWID Signature Method not yet tested");
+            log.Warning("SWID Signature Method not yet tested");
             return false;
         }
     }
