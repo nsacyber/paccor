c8535dee43aa547e436a5b85d0d85ffc8d19d3f9
5B96790E3664F40075A67E6ADF737EDB15B4408DBC91A81228B31537B0CE3E26 <33426478+iadgovuser29@users.noreply.github.com>
2025-06-17 13:52:23 -0400

    Nvme json parsing (#122)
    * Update nvme script to handle bug in nvme-cli json
    * ComponentIdentifierV2Factory don't trim serial or revision

--- scripts/nvme.sh
+++ scripts/nvme.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Gather descriptors for NVMe devices  
 nvmeParse () {
-    str=$(nvme list -o json)
+    str=$(nvme list -o json -v)
     count=$(echo "$str" | jq '.Devices | length')
     nvmeDevices=()
 
     for ((i = 0 ; i < count ; i++ )); do
-        elementJson=$(echo "$str" | jq .Devices["$i"])
-        nvmeDevices+=("$elementJson")        
+        elementJson=$(echo "$str" | jq -r .Devices["$i"].Subsystems[0].Controllers[0].Controller)
+        elementJson="/dev/$elementJson"
+        nvmeDevices+=("$elementJson") 
     done
 }
 nvmeNumDevices () {
@@ -15,23 +16,21 @@ nvmeNumDevices () {
 }
 nvmeGetModelNumberForDevice () {
     dev="${1}"
-    str=$(echo "${nvmeDevices[$dev]}" | jq -r .ModelNumber)
+    str=$(nvme id-ctrl "${nvmeDevices[$dev]}" -o json | jq -r .mn)
     printf "%s" "$str"
 }
 nvmeGetSerialNumberForDevice () {
     dev="${1}"
-    str=$(echo "${nvmeDevices[$dev]}" | jq -r .SerialNumber)
+    str=$(nvme id-ctrl "${nvmeDevices[$dev]}" -o json | jq -r .sn)
     printf "%s" "$str"
 }
 nvmeGetEuiForDevice () {
     dev="${1}"
-    devNode=$(echo "${nvmeDevices[$dev]}" | jq -r .DevicePath)
-    str=$(nvme id-ns "$devNode" -o json | jq -r .eui64)
+    str=$(nvme id-ns "${nvmeDevices[$dev]}n1" -o json | jq -r .eui64)
     printf "%s" "$str"
 }
 nvmeGetNguidForDevice () {
     dev="${1}"
-    devNode=$(echo "${nvmeDevices[$dev]}" | jq -r .DevicePath)
-    str=$(nvme id-ns "$devNode" -o json | jq -r .nguid)
+    str=$(nvme id-ns "${nvmeDevices[$dev]}n1" -o json | jq -r .nguid)
     printf "%s" "$str"
 }
