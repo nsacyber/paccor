6f362963b902430df7175705eebcf1b2e5c938c1
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2026-09-17 13:45:06 -0400

    Add SHA-1 warning to IssuerCertificateChecker.validateSignature

--- src/main/java/paccor/crypto/IssuerCertificateChecker.java
+++ src/main/java/paccor/crypto/IssuerCertificateChecker.java
@@ -26,6 +26,9 @@ public final class IssuerCertificateChecker {
 
     public boolean validateSignature(PlatformCertificate platform, X509CertificateHolder issuer) {
         try {
+            if (platform != null && AlgorithmSupport.isSha1Signature(platform.extractSignatureAlgorithm())) {
+                LOGGER.warning("WARNING: Validating a certificate that uses SHA-1; SHA-1 is deprecated and should only be used for legacy compatibility.");
+            }
             ContentVerifierProvider verifier = SignatureService.buildWithDefault(issuer);
             return platform != null && platform.isSignatureValid(verifier);
         } catch (OperatorCreationException | CertException e) {
